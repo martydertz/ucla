ba0b1d5802e7048da1056441fbc480c6de2676c4
martin dertz <martydertz@gmail.com>
2017-03-03 17:56:58 -0800

    Update matching_functions.r

--- id_matching/matching_functions.r
+++ id_matching/matching_functions.r
@@ -1,25 +1,33 @@
 ##Match on email -- for every email provided check all TOAD and CRM emails. Only use unique matches
-matchEmails <- function(df, emailColumns = c("Email")){
+matchEmails <- function(missingDataFrame, refDataCurrent, refDataEmails, emailCols = c("Email")){
   #For each missing record
-  for(i in seq(dim(df)[[1]])){
+  numEmails <- length(emailCols)
+  for(i in seq(dim(missingDataFrame)[[1]])){
     found <- FALSE
+    curEmail <-0
     #For each email provided
-    for(columnName in emailColumns){
-      if(!found){
-        email <- as.character(missing[i, columnName])
+    while(!found & curEmail < numEmails){
+        curEmail <- curEmail + 1
+        colName <- emailCols[curEmail]
+        email <- as.character(missingDataFrame[i, colName])
         # Any current email matches
-        m <- subset(alumni, toad_b_email == email  | toad_h_email == email | toad_p_email == email |
+        m <- subset(refDataCurrent, toad_b_email == email  | toad_h_email == email | toad_p_email == email |
                       crm_h_email == email | crm_b_email == email |  anderson_email == email)
-        if(dim(m)[1] == 0){
-          m <- subset(all_emails, old_email == email | new_email == email)
-        }
         if(dim(m)[1]== 1){
-          missing[i,'CRM.ID'] = m[['id']]
-          missing[i, 'Match.Method'] = 'Email Matched'
+          missingDataFrame[i,'CRM.ID'] = m[['id']]
+          missingDataFrame[i, 'Match.Method'] = 'Email Matched'
           found <- TRUE
+          }
+        if(!found){
+          m <- subset(refDataEmails, old_email == email | new_email == email)
+          if(dim(m)[1]== 1){
+            missingDataFrame[i,'CRM.ID'] = m[['id']]
+            missingDataFrame[i, 'Match.Method'] = 'Email Matched'
+            found <- TRUE
         }
-      }
+        }
     }
   }
+  return(missingDataFrame)
 }
 
