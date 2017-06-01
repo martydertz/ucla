b3fff4a728d8cc94c7ee2bcb8550eb26e774c7f2
martin dertz <martydertz@gmail.com>
2017-06-01 07:57:42 -0700

    Update widening.r

--- empl_reshape/widening.r
+++ empl_reshape/widening.r
@@ -1,6 +1,5 @@
 
 df <- read.csv('all_and_empl.csv')
-
 names(df) <- gsub("Relationships.Employment.Information.", "", names(df))
 names(df) <- gsub("Relationships.Related.Constituent.", "", names(df))
 names(df)[1] <- "Full.Name"
@@ -19,9 +18,15 @@ sp_df <- split(p_emp, p_emp$Lookup.ID)
 d <- sp_df[[1]]
 d <- d[,c("Lookup.ID", "Job.title", "End.date", "Start.date", "Lookup.ID.1", "Name")]
 d <-plyr::rename(d,replace= c("Job.title"="Former.Title") )
-comb <- head(merge( cur_emp,d, by.x="Lookup.ID", by.y="Lookup.ID", all=TRUE))
+comb <- merge( cur_emp,d, by.x="Lookup.ID", by.y="Lookup.ID", all=TRUE)
 sp_df[4]
 
-
-
-head(df[order(df$Start.datef, decreasing = TRUE),])
+for(i in seq(1:length(sp_df)){
+    emp <- sp_df[[i]][,c("Lookup.ID", "Job.Title", "End.date", "Start.date", "Name")]
+    emp <- emp[order(emp$End.date, decreasing=FALSE)]
+    if(dim(emp[1] > 0)){
+        for(m in 1:nrow(emp)){
+            emp.i <- emp[i,]
+        }
+    }
+}
